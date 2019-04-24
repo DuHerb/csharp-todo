c1e9d5f846a0a0d38b69de79535ecc34a0fa11c2
Dustin Herboldshimer and Heather Zurek <example@example.com>
2019-04-24 11:29:02 -0700

    use a while loop

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -11,37 +11,39 @@ namespace ToDoList
     {
 
       Program newProgram = new Program();
-      string userChoice = newProgram.GetUserChoice();
-
-      if (userChoice == "1")
+      Console.WriteLine("Welcome To Your ToDo List");
+      bool isUsing = true;
+      while (isUsing)
       {
-        List<Item> todoList =  Item.GetAll();
-        foreach(Item todo in todoList)
+        string userChoice = newProgram.GetUserChoice();
+
+        if (userChoice == "1")
         {
-          Console.WriteLine(todo.GetDescription());
+          Console.WriteLine("-------");
+          List<Item> todoList =  Item.GetAll();
+          foreach(Item todo in todoList)
+          {
+            Console.WriteLine(todo.GetDescription());
+          }
+          Console.WriteLine("-------");
+          Console.WriteLine("Would you like to Exit: Press Y");
+          if(Console.ReadLine() == "y" || Console.ReadLine() == "Y")
+          {
+            isUsing = false;
+          }
         }
-      }
-      else if(userChoice == "2")
-      {
-        Console.WriteLine("Type in your Todo item: ");
-        string newDescription = Console.ReadLine();
-        Item newItem = new Item(newDescription);
-      }
+        else if(userChoice == "2")
+        {
+          Console.WriteLine("Type in your Todo item: ");
+          string newDescription = Console.ReadLine();
+          Item newItem = new Item(newDescription);
+        }
 
-      Console.WriteLine("Would you like to Exit: Press Y");
-      if(Console.ReadLine() == "y" || Console.ReadLine() == "Y")
-      {
-        Environment.Exit(0);
-      }
-      else
-      {
-        Program.Main();
       }
     }
 
     public string GetUserChoice()
     {
-      Console.WriteLine("Welcome To Your ToDo List");
       Console.WriteLine("To view your ToDo List, press 1");
       Console.WriteLine("To add an item to your list, press 2");
       string choice = Console.ReadLine();
